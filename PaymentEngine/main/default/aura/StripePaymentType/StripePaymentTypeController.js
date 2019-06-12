ac8368b5a96d6d2634d4bf4194afd9d427e10305
Gurp Parmar <parmarg@gmail.com>
2019-06-12 17:25:15 -0400

    updated

--- PaymentEngine/main/default/aura/StripePaymentType/StripePaymentTypeController.js
+++ PaymentEngine/main/default/aura/StripePaymentType/StripePaymentTypeController.js
@@ -4,30 +4,36 @@
         helper.buildYearOptions(component);
     },
     processPayment : function (component,event,helper) {
+        if (component.find('full_name').validate() && component.find('cc').validate() && component.find('month').validate()
+            && component.find('year').validate()) {
 
-        var postData = 'card[number]='+component.get('v.creditCardObj.cc')+'&card[exp_month]='+component.get('v.creditCardObj.month');
-        postData += '&card[exp_year]='+component.get('v.creditCardObj.year')+'&card[cvc]=343&key=pk_test_NCuV8goKtHrOhfspBo61LU6e00bIsJQkWG';
-        fetch('https://api.stripe.com/v1/tokens', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            },
-            body: postData
-        }).then($A.getCallback(function(res){
-            res.json().then(function(result){
-                var paymentObj = {
-                    record : component.get('v.customPaymentTypeObj.paymentObj.paymentObjId'),
-                    paymentMethodToken : result.id,
-                    currencyISOCode : component.get('v.customPaymentTypeObj.paymentObj.currencyISOCode'),
-                }
-                ActionUtils.executeAction(helper,component,'c.processStrPayment',paymentObj)
-                    .then(function(result){
-                        window.location = '/'+result.receiptId;
-                    })
-                    .catch(function(result){
-                        helper.toggleButtonEvent('paymentButtons');
-                    });
-            });
-        }));
+            var postData = 'card[number]=' + component.get('v.creditCardObj.cc') + '&card[exp_month]=' + component.get('v.creditCardObj.month');
+            postData += '&card[exp_year]=' + component.get('v.creditCardObj.year') + '&card[cvc]=343&key=pk_test_NCuV8goKtHrOhfspBo61LU6e00bIsJQkWG';
+            fetch('https://api.stripe.com/v1/tokens', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                body: postData
+            }).then($A.getCallback(function (res) {
+                res.json().then(function (result) {
+                    var paymentObj = {
+                        record: component.get('v.customPaymentTypeObj.paymentObj.paymentObjId'),
+                        paymentMethodToken: result.id,
+                        currencyISOCode: component.get('v.customPaymentTypeObj.paymentObj.currencyISOCode'),
+                    }
+                    ActionUtils.executeAction(helper, component, 'c.processStrPayment', paymentObj)
+                        .then(function (result) {
+                            window.location = '/' + result.receiptId;
+                        })
+                        .catch(function (result) {
+                            helper.toggleButtonEvent('paymentButtons');
+                        });
+                });
+            }));
+        }
+        else {
+            helper.toggleButtonEvent('paymentButtons');
+        }
     }
 })
